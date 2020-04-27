ff744ae9c3752ccf991c22c3cba9e13823552200
Edwin Daniel Baltazar Terbullino <edwin.baltazar@tecsup.edu.pe>
2020-04-27 14:37:51 -0500

    first commit

---
+++ Labo5/Labo5/ButtonCode.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Labo5
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ButtonCode : ContentPage
+	{
+		public ButtonCode ()
+		{
+			InitializeComponent ();
+		}
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await label.RelRotateTo(360, 1000);
+        }
+    }
+}
